210b1aa26c93c0ff35f7b6ae2ba946d844a3b644
HenriqueDeBritto <pradohenrique162@gmail.com>
2024-05-07 00:10:13 -0300

    estudo de if else

--- JAVAZIN/src/edu/henrique/mesum/variaveis/EstudoDeVariaveis.java
+++ JAVAZIN/src/edu/henrique/mesum/variaveis/EstudoDeVariaveis.java
@@ -2,6 +2,18 @@ package edu.henrique.mesum.variaveis;
 
 public class EstudoDeVariaveis {
     public static void main(String[]args){
-        System.out.println("bananas");
+        boolean numiro = true;
+        boolean numirodoi = false;
+
+        System.out.println("Bo dia");
+        if (numiro) {
+            if(numirodoi){
+                System.out.println("ambos são verdadeiros");
+            }else{
+                System.out.println("numirodoi é falso");
+            }
+        }else{
+            System.out.println("numiro não é verdadeiro");
+        }
     }
 }
